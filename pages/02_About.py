6dfcfcccfd812aeea9ae64604a89f878859afa72
QulubilHamdiAU <80740505+QulubilHamdiAU@users.noreply.github.com>
2024-01-08 18:59:56 +0700

    Update 02_About.py

--- pages/02_About.py
+++ pages/02_About.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 st.sidebar.markdown("# About")
 
-st.sidebar.markdown("This is a AIFAQ chatbot designed to provide information about student admission. "
+st.sidebar.markdown("This is a AIFAQ chatbot designed to provide information about new student admission. "
                     "Feel free to ask questions, and the chatbot will respond accordingly.")
 
 st.title("Chatbot")
-st.markdown("Welcome to AIFAQ! This chatbot is designed to provide information about student admission. ")
+st.markdown("Welcome to AIFAQ! This chatbot is designed to provide information about new student admission. ")
 
 st.markdown("### How to Use")
-st.markdown("1. Start by typing your question or instruction in the chat input box.")
+st.markdown("1. Start by typing your question in the chat input box.")
 st.markdown("2. The chatbot will respond with relevant information or advice based on your input.")
-st.markdown("3. Feel free to continue the conversation by asking follow-up questions or providing additional instructions.")
+st.markdown("3. Feel free to continue the conversation by asking follow-up questions.")
 st.markdown("4. Use the 'Clear Session' button to start a new conversation.")
